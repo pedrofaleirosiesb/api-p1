23e5e392797e1753d3e8f146dd0e8118f6237874
pedrofaleiros <pedrocrfaleiros@yahoo.com.br>
2021-09-24 09:45:00 -0300

    Initial commit

---
+++ src/main/java/br/iesb/poo/apip1/ApiP1Application.java
@@ -0,0 +1,13 @@
+package br.iesb.poo.apip1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiP1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiP1Application.class, args);
+    }
+
+}
---
+++ src/test/java/br/iesb/poo/apip1/ApiP1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.iesb.poo.apip1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiP1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
